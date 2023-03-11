be760ee725eeb5780ac64bb584ad34523b2bc66d
Ahmed Alwardani <ahmedalwardani@Ahmeds-MacBook-Pro.local>
2023-03-11 15:47:21 -0500

    add containsDuplicate

--- arrays/containsDuplicate.js
+++ arrays/containsDuplicate.js
@@ -1,1 +1,16 @@
 //Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+let containsDuplicate = (nums) => {
+  let containsDuplicate = false;
+  let containsObject = {};
+  for (const num of nums) {
+    if (containsObject[num]) {
+      containsDuplicate = true;
+    } else {
+      containsObject[num] = true;
+    }
+  }
+  return containsDuplicate;
+};
+
+console.log(containsDuplicate([0, 1, 0, 3, 12]));
+console.log(containsDuplicate([1, 2, 3]));
