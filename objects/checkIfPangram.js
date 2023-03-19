2b2e84b6214160e80b5ce7e780e22e081c5b8dad
Ahmed Alwardani <aalwa049@uottawa.ca>
2023-03-19 19:04:39 -0400

    add checkIfPangram.js

---
+++ objects/checkIfPangram.js
@@ -0,0 +1,13 @@
+// A pangram is a sentence where every letter of the English alphabet appears at least once.
+
+// Given a string sentence containing only lowercase English letters, return true if sentence is a pangram, or false otherwise.
+
+const checkIfPangram = (sentence) => {
+  const containsLetter = {};
+  for (const letter of sentence) {
+    if (!containsLetter[letter]) {
+      containsLetter[letter] = true;
+    }
+  }
+  return Object.keys(containsLetter).length === 26 ? true : false;
+};
