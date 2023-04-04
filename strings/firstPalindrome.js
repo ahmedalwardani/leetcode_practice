c01c828d25a289786637a17bc8896d111f2e0457
Ahmed Alwardani <aalwa049@uottawa.ca>
2023-04-03 23:04:36 -0400

    add firstPalindrome

---
+++ strings/firstPalindrome.js
@@ -0,0 +1,12 @@
+// Given an array of strings words, return the first palindromic string in the array. If there is no such string, return an empty string "".
+
+// A string is palindromic if it reads the same forward and backward.
+
+const firstPalindrome = (words) => {
+  for (const word of words) {
+    if (word === word.split("").reverse().join("")) {
+      return word;
+    }
+  }
+  return "";
+};
