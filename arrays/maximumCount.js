6afaa92815edd3d89a9bce7320b4c401ae7436c6
Ahmed Alwardani <aalwa049@uottawa.ca>
2023-04-16 16:35:47 -0400

    add maximumCount.js

---
+++ arrays/maximumCount.js
@@ -0,0 +1,18 @@
+// Given an array nums sorted in non-decreasing order, return the maximum between the number of positive integers and the number of negative integers.
+
+// In other words, if the number of positive integers in nums is pos and the number of negative integers is neg, then return the maximum of pos and neg.
+// Note that 0 is neither positive nor negative.
+
+const maximumCount = (nums) => {
+  let positiveNumbersCounter = 0;
+  let neagativeNumbersCounter = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] > 0) positiveNumbersCounter += 1;
+    else if (nums[i] < 0) neagativeNumbersCounter += 1;
+    else continue;
+  }
+  return positiveNumbersCounter >= neagativeNumbersCounter
+    ? positiveNumbersCounter
+    : neagativeNumbersCounter;
+};
