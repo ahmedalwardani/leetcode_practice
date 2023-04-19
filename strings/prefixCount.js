1bc75b08aae18aa8cc28a1778ecf851ad74dc796
Ahmed Alwardani <aalwa049@uottawa.ca>
2023-04-18 23:22:33 -0400

    add prefixCount with indexOf

--- strings/prefixCount.js
+++ strings/prefixCount.js
@@ -13,3 +13,11 @@ const prefixCount = (words, pref) => {
   }
   return counter;
 };
+
+var prefixCountWitnIndexOf = function (words, pref) {
+  let count = 0;
+  for (let word of words) {
+    if (word.indexOf(pref) == 0) count++;
+  }
+  return count;
+};
