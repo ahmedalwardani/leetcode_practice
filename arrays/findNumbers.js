f0f6ed867ef59d1cd48b8838f7185220f1017c71
Ahmed Alwardani <aalwa049@uottawa.ca>
2023-05-15 21:37:48 -0400

    add findNumbers

---
+++ arrays/findNumbers.js
@@ -0,0 +1,9 @@
+// Given an array nums of integers, return how many of them contain an even number of digits.
+
+const findNumbers = (nums) => {
+  const numsAsString = nums.map(String);
+  return numsAsString.filter((numAsString) => numAsString.length % 2 === 0)
+    .length;
+};
+
+console.log(findNumbers([12, 345, 2, 6, 7896]));
