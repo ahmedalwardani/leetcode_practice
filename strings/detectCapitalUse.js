00925fa6ac977bfdef2852a60ec783a1e1a8b366
Ahmed Alwardani <aalwa049@uottawa.ca>
2023-07-01 21:20:37 -0400

    add detectCapitalUse

---
+++ strings/detectCapitalUse.js
@@ -0,0 +1,13 @@
+// We define the usage of capitals in a word to be right when one of the following cases holds:
+
+// All letters in this word are capitals, like "USA".
+// All letters in this word are not capitals, like "leetcode".
+// Only the first letter in this word is capital, like "Google".
+// Given a string word, return true if the usage of capitals in it is right.
+
+const detectCapitalUse = (word) => {
+  if (word.toLowerCase() === word || word.toUpperCase() === word) return true;
+  if (`${word[0].toUpperCase()}${word.slice(1).toLowerCase()}` === word)
+    return true;
+  return false;
+};
