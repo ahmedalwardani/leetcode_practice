9131e283e0dce82820eb2ca7ae0a357736ac8a3a
Ahmed Alwardani <aalwa049@uottawa.ca>
2023-07-04 19:26:25 -0400

    maxPower

---
+++ strings/maxPower.js
@@ -0,0 +1,21 @@
+// The power of the string is the maximum length of a non-empty substring that contains only one unique character.
+
+// Given a string s, return the power of s.
+
+const maxPower = (s) => {
+  let maxStr = 1;
+  let accum = 0;
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === s[i + 1]) {
+      maxStr += 1;
+    } else {
+      maxStr = 1;
+    }
+    if (maxStr > accum) {
+      accum = maxStr;
+    }
+    console.log(maxStr);
+    console.log(accum);
+  }
+  return accum;
+};
